37aa45b6996ef10f8ca3a66bef5752a46e3cc0df
Mohammad Ridwanur Rahman <ridwanur13@gmail.com>
2025-01-05 19:02:29 +0600

    completed socially app

---
+++ src/app/profile/[username]/loading.tsx
@@ -0,0 +1,4 @@
+function loading() {
+  return <div>Loading... We are fetching your profile, please wait</div>;
+}
+export default loading;
---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,45 @@
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile.action";
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const user = await getProfileByUsername(params.username);
+
+  if (!user) return;
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.name}'s profile`,
+  };
+}
+
+async function ProfilePageServer({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return (
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
+}
+export default ProfilePageServer;
